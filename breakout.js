3e152bd197d92758002d78ad220ec55403bb4a54
oczoro <44875926+oczoro@users.noreply.github.com>
2018-11-15 11:09:58 -0600

    breakout update

--- breakout.js
+++ breakout.js
@@ -1,13 +1,9 @@
 var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
 var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
 var paddleHeight = 10;
 var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 var brickRowCount = 5;
@@ -19,6 +15,10 @@ var brickOffsetTop = 30;
 var brickOffsetLeft = 30;
 var score = 0;
 var lives = 3;
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var paddleX = (canvas.width-paddleWidth)/2;
 
 var bricks = [];
 for(var c=0; c<brickColumnCount; c++) {
